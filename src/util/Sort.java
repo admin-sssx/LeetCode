b2b09d7fb0f2bd5c4ee919a9c97ca9bf33b250eb
sssx <li2541220@163.com>
2019-06-11 15:02:30 +0800

    sort

--- src/util/Sort.java
+++ src/util/Sort.java
@@ -5,6 +5,31 @@ public class Sort {
     int arr[] = new int[size];
 
     public static void maoPao(int arr[]){
+        Srand srand = new Srand();
+        System.out.println("----------------source data-------------------");
+        srand.showArr(arr);
+        System.out.println("----------------maopao start-------------------");
+        for (int i = 0; i <arr.length-1 ; i++) {
 
+            for (int j = 0; j < arr.length-i-1; j++) {
+
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            srand.showArr(arr);
+
+        }
+        System.out.println("----------------maopao end-------------------");
+
+    }
+
+    public static void quickSort(int arr[]){
+        Srand srand = new Srand();
+        System.out.println("----------------source data-------------------");
+        srand.showArr(arr);
+        System.out.println("----------------quick start-------------------");
     }
 }
